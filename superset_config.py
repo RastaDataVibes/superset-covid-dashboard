259dff437c8e9e0545879162dc6a5e121a876636
RastaDataVibes <opiobethle@gmail.com>
2025-08-11 17:48:08 +0300

    Initial Superset setup

--- superset_config.py
+++ superset_config.py
@@ -1,4 +1,5 @@
 SECRET_KEY = 'JiI87ouhSQSxVdsZO/nLXVSkGC5DhbWLe5CLpEFN1nwPeZb1BMSC525V'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Isle%40opio1397@192.168.1.12:3306/covid_dashboard'
+SQLALCHEMY_DATABASE_URI = 'postgresql://greenchain_admin:MTvRpQRtD2c5yIpQplT9dDDgXRC6BhYm@dpg-d285uac9c44c73a3sa70-a.frankfurt-postgres.render.com:5432/greenchain_db'
+
 
 
