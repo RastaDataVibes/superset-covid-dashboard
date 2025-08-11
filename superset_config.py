cc8d5ea96c8cfd387faa3bab01ad8060673cac4f
RastaDataVibes <opiobethle@gmail.com>
2025-08-11 18:55:35 +0300

    Point Superset to Render PostgreSQL database

--- superset_config.py
+++ superset_config.py
@@ -1,5 +1,13 @@
 SECRET_KEY = 'JiI87ouhSQSxVdsZO/nLXVSkGC5DhbWLe5CLpEFN1nwPeZb1BMSC525V'
-SQLALCHEMY_DATABASE_URI = 'postgresql://greenchain_admin:MTvRpQRtD2c5yIpQplT9dDDgXRC6BhYm@dpg-d285uac9c44c73a3sa70-a.frankfurt-postgres.render.com:5432/greenchain_db'
+
+import os
+
+SQLALCHEMY_DATABASE_URI = os.getenv(
+    "DATABASE_URL",
+    "postgresql://greenchain_admin:MTvRpQRtD2c5yIpQplT9dDDgXRC6BhYm@dpg-d285uac9c44c73a3sa70-a.frankfurt-postgres.render.com:5432/greenchain_db"
+)
+
+
 
 
 
