b5cf9948de9d5a215039350dbd07017bec923176
RastaDataVibes <opiobethle@gmail.com>
2025-08-11 15:09:53 +0300

    Update DB host to local IP for Render deployment

--- superset_config.py
+++ superset_config.py
@@ -1,3 +1,4 @@
 SECRET_KEY = 'JiI87ouhSQSxVdsZO/nLXVSkGC5DhbWLe5CLpEFN1nwPeZb1BMSC525V'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Isle%40opio1397@host.docker.internal:3306/covid_dashboard'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Isle%40opio1397@192.168.1.12:3306/covid_dashboard'
+
 
