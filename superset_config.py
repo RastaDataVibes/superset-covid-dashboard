27ebd31b1f1a1ef3521da68191aa058403f7d459
RastaDataVibes <opiobethle@gmail.com>
2025-08-13 15:56:24 +0300

    Increase Superset query timeout

--- superset_config.py
+++ superset_config.py
@@ -7,6 +7,10 @@ SQLALCHEMY_DATABASE_URI = os.getenv(
     "postgresql://greenchain_admin:MTvRpQRtD2c5yIpQplT9dDDgXRC6BhYm@dpg-d285uac9c44c73a3sa70-a.frankfurt-postgres.render.com:5432/greenchain_db"
 )
 
-
 ENABLE_EXAMPLES = False
+
+# Increase query timeout
+SQLLAB_TIMEOUT = 300  # SQL Lab queries
+SUPERSET_WEBSERVER_TIMEOUT = 300  # Dashboard/chart queries
+SQLLAB_ASYNC_TIME_LIMIT_SEC = 300
 
