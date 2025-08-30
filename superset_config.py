904ac95a6a706a767deac03c9f72fbb3499c48f7
RastaDataVibes <opiobethle@gmail.com>
2025-08-31 01:02:26 +0300

    Secure superset_config.py using environment variables

--- superset_config.py
+++ superset_config.py
@@ -1,16 +1,9 @@
-SECRET_KEY = 'JiI87ouhSQSxVdsZO/nLXVSkGC5DhbWLe5CLpEFN1nwPeZb1BMSC525V'
-
 import os
 
-SQLALCHEMY_DATABASE_URI = os.getenv(
-    "DATABASE_URL",
-    "postgresql://greenchain_admin:MTvRpQRtD2c5yIpQplT9dDDgXRC6BhYm@dpg-d285uac9c44c73a3sa70-a.frankfurt-postgres.render.com:5432/greenchain_db"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 ENABLE_EXAMPLES = False
-
-# Increase query timeout
-SQLLAB_TIMEOUT = 300  # SQL Lab queries
-SUPERSET_WEBSERVER_TIMEOUT = 300  # Dashboard/chart queries
+SQLLAB_TIMEOUT = 300
+SUPERSET_WEBSERVER_TIMEOUT = 300
 SQLLAB_ASYNC_TIME_LIMIT_SEC = 300
-
